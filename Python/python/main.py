a0963e8c474ecd6a17f10adade289e5fabb8a3cd
João Correia <147398180+JoaoCorreia2008@users.noreply.github.com>
2024-10-22 12:31:13 +0100

    Add files via upload

---
+++ Python/python/main.py
@@ -0,0 +1,11 @@
+# import modules as m # faz a mesma coisa que o import modules mas muda o nome  
+# import modules # importa as funções que estão no ficheiro modules.py
+from modules import mult, soma # importa funções especificas do outro ficheiro 
+from sys import platform #sys é uma livraria do python 
+
+boasvindas = "Ola python"
+print(soma(3,4))
+
+
+platform = "Minha plataforma"
+print(platform)
---
+++ Python/python/modules.py
@@ -0,0 +1,7 @@
+def soma (x,y):
+    return x + y
+
+def mult(x,y):
+    return x * y
+
+# se der um print aqui no outro ficheiro que chamei as funções deste ficheiro vai dar o print 
